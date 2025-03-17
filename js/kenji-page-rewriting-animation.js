eb730ed5acf838162e24265dcced3305a57a34f0
Robert Zohrabyan <116836484+RobertZohrabyan@users.noreply.github.com>
2025-03-17 10:49:54 +0100

    Delete js directory

--- js/kenji-page-rewriting-animation.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-        var typed = new Typed(".skills", {
-                strings: ["TeleRobotics Engineering", "Robotics Development", "Robodrive"],
-                typeSpeed: 100,
-                backSpeed: 100,
-                backDelay: 100,
-                loop: true
-            });
- });
